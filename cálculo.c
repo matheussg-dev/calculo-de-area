11f1452ae0bbdf0ddff523650e2d15e4aa0cf05b
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-23 15:54:38 -0300

    calculo

---
+++ cálculo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("progama para cálculo da área da casa\n");
+    printf("A área da sala é %i\n ,10*10")
+    printf("A área do banheiro é %i\n ,5*7")
+    printf("A área do banheiro é %i\n ,5*7")
+    printf("A área total é %i\n ,10*10+5*7+5*7");
+    return 0;
+}
