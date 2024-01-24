2b56aa6d1c666d3166750de6a764c0fe39e7ec73
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-24 15:31:59 -0300

    adicionando a picina na variavel do calculo

--- cálculo.c
+++ cálculo.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <math.h>
+
 int main() {
     float lateral = 10;
     float cquarto = 7;
     float areaq;
     float areas;
     float areat;
-// float e uma variavel que serve para mostrar os decimais
-    printf("progama para cálculo da área da casa\n");
+    double raio = 2;
+    double areap;
+
+// float e uma variavel que serve para mostrar os 7 dígitos decimais
+// double tem 2x a precisão de float, geralmente possui 15 dígitos decimais
+
+    printf("progama para cálculo da área da casa\n") ;
     areas = lateral*lateral ;
-    printf("A área da sala é %f\n" , areas);
+    printf("A área da sala é %f\n" , areas) ;
     areaq = cquarto*(lateral/2);
-    printf("A área do quarto é %f\n" , areaq);
-    printf("A área do banheiro é %f\n" , areaq);
+    printf("A área do quarto é %f\n" , areaq) ;
+    printf("A área do banheiro é %f\n" , areaq) ;
     areat = areas + 2*areaq;
-    printf("A área total é %f\n" , areat);
+    printf("A área total é %f\n" , areat) ;
+    areap = M_PI * pow(raio,2) ;
+    printf("A área da picina é %f\n" , areap)
     return 0;
 } 
