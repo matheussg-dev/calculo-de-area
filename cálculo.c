d2becf8bcc252ea447c9ecb6ee8b24c08d47d645
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-24 14:03:31 -0300

    evolução do cálculo e da logica

--- cálculo.c
+++ cálculo.c
@@ -4,6 +4,6 @@ int main() {
     printf("A área da sala é %i\n ,10*10")
     printf("A área do banheiro é %i\n ,5*7")
     printf("A área do banheiro é %i\n ,5*7")
-    printf("A área total é %i\n ,10*10+5*7+5*7");
+    printf("A área total é %i\n ,10*10 + 5*7 + 5*7");
     return 0;
 }
