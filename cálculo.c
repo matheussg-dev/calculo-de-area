a00d5d3b7f5aea7b2b04b88c79ed907a7e58c360
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-24 16:28:37 -0300

    separando o calculo teste

--- cálculo.c
+++ cálculo.c
@@ -2,16 +2,14 @@
 #include <math.h>
 
 int main() {
-    float lateral = 10;
-    float cquarto = 7;
+    areaCASA(11,7)
+}
+
+void areaCASA(float lateral, float cquarto){
+
     float areaq;
     float areas;
     float areat;
-    double raio = 2;
-    double areap;
-
-// float e uma variavel que serve para mostrar os 7 dígitos decimais
-// double tem 2x a precisão de float, geralmente possui 15 dígitos decimais
 
     printf("progama para cálculo da área da casa\n") ;
     areas = lateral*lateral ;
@@ -21,7 +19,23 @@ int main() {
     printf("A área do banheiro é %f\n" , areaq) ;
     areat = areas + 2*areaq;
     printf("A área total é %f\n" , areat) ;
-    areap = M_PI * pow(raio,2) ;
-    printf("A área da picina é %f\n" , areap)
+}
+
+double areaPiscina(double raio){
+    return M_PI*pow(raio,2);
+}
+
+int main() {
+
+    double areap;
+    areaCASA();
+
+    areap = areaPiscina(2);
+    printf("A área da picina é %f\n" , areap);
+
     return 0;
 } 
+
+// float e uma variavel que serve para mostrar os 7 dígitos decimais
+// double tem 2x a precisão de float, geralmente possui 15 dígitos decimais
+// separando o calculo da casa e da picina
