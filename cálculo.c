b4bd3e72a6e85fd047bc9a2740f28fe0719f6c39
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-29 13:58:24 -0300

    separando o calculo final correcao

--- cálculo.c
+++ cálculo.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    float lateral = 10;
-    float cquarto = 7;
+void areaCASA(float lateral, float cquarto){
     float areaq;
     float areas;
     float areat;
-    double raio = 2;
+    if (lateral < 0) {
+        printf("Erro: lateral da sala < 0\n");
+    } else {
+        if (cquarto < 0) {
+            printf("Erro: lateral do quarto < 0\n");
+        } else {
+            printf("Programa para calculo da area da casa\n");
+            areas = lateral * lateral;
+            printf("A area da sala e %f\n", areas);
+            areaq = cquarto * (lateral / 2);
+            printf("A area do quarto e %f\n", areaq);
+            printf("A area do banheiro e %f\n", areaq);
+            areat = areas + 2 * areaq;
+            printf("A area total e %f\n", areat);
+        }
+    }
+}
+
+double areaPiscina(double raio) {
+    return M_PI * pow(raio, 2);
+}
+
+int main() {
+    float lateral = 11;
+    float cquarto = 7;
     double areap;
 
-    printf("programa para calculo da area da casa\n");
+    areaCASA(lateral, cquarto);
 
-    areas = lateral * lateral;
-    printf("A area da sala e %f\n", areas);
-
-    areaq = cquarto * (lateral / 2);
-    printf("A area do quarto e %f\n", areaq);
-    printf("A area do banheiro e %f\n", areaq);
-
-    areat = areas + 2 * areaq;
-    printf("A area total e %f\n", areat);
-
-    areap = M_PI * pow(raio, 2);
+    areap = areaPiscina(2.0);
     printf("A area da piscina e %f\n", areap);
 
     return 0;
 }
+int main() {
+
+    double areap;
+    areaCASA();
+
+    areap = areaPiscina(2);
+    printf("A area da picina e %f\n" , areap);
+
+    return 0;
+} 
 
 // float e uma variavel que serve para mostrar os 7 digitos decimais
 // double tem 2x a precisao de float, geralmente possui 15 digitos decimais
