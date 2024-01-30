afa213845b236560d2582f4e85f96c9475dc36e2
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-30 17:33:46 -0300

    mudanca de ideia

--- cálculo.c
+++ cálculo.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-
-
 void areaPiscina(double raio) {
-    
     
     double areap = M_PI * pow(raio, 2);
     
@@ -14,13 +11,7 @@ void areaPiscina(double raio) {
     double novaAreaP = (double)(calculoP/100);
     
     printf("A area da piscina simplificada e %f\n", novaAreaP);
-    
-    double valorM2 = 1500; //valor do metro quadrado
-    double valor = (novaAreaP*valorM2);
-        
-    printf("O preco da piscina e de R$ %f\n", valor);
-    
-   
+      
 }
 
 void areaCasa(float lateral, float cquarto) {
@@ -33,28 +24,30 @@ void areaCasa(float lateral, float cquarto) {
     if (lateral < 0) {
         printf("Erro: lateral da sala < 0\n");
     } else {
-        if (cquarto < 0) {
-            printf("Erro: lateral do quarto < 0\n");
-        } else {
-            printf("Programa para calculo da area da casa\n");
-            areas = lateral * lateral;
-            printf("A area da sala e %f\n", areas);
-            areaq = cquarto * (lateral / 2);
-            printf("A area do quarto e %f\n", areaq);
-            printf("A area do banheiro e %f\n", areaq);
-            areat = areas + 2 * areaq;
-            printf("A area total e %f\n", areat);
+            if (cquarto < 0) {
+                printf("Erro: lateral do quarto < 0\n");
+            } else {
+                printf("Programa para calculo da area da casa\n");
+                areas = lateral * lateral;
+                printf("A area da sala e %f\n", areas);
+                areaq = cquarto * (lateral / 2);
+                printf("A area do quarto e %f\n", areaq);
+                printf("A area do banheiro e %f\n", areaq);
+                areat = areas + 2 * areaq;
+                printf("A area total e %f\n", areat);
         }
-   }
-    
-   double valorM2 = 1500;
-    double valor = (areat*valorM2);
-        
-    printf("O preco da casa e de R$ %f\n", valor);
-    
+    }
 }
 
 int main() {
+    
+    double valorM2 = 1500; //valor do metro quadrado
+    double valorC = (areat*valorM2);
+    double valorP = (novaAreaP*valorM2);
+    double valorT = (valorC+valorP)
+    
+    printf("O preco da piscina e de R$ %f\n", valorP);
+    printf("O preco da piscina e de R$ %f\n", valorC);
     
     areaCasa(11,7);
     areaPiscina(2);
